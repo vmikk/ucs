2e264a5eae9d9249f9439c4ec760678a51e3351d
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-05-27 12:06:06 +0300

    decompose into functions, add error handling

--- ucs.go
+++ ucs.go
@@ -10,52 +10,69 @@ import (
 )
 
 func main() {
-	inputFile := flag.String("input", "", "Input file (default: stdin)")
-	outputFile := flag.String("output", "", "Output file (default: stdout)")
-	flag.StringVar(inputFile, "i", "", "Input file (default: stdin)")
-	flag.StringVar(outputFile, "o", "", "Output file (default: stdout)")
+	inputFile, outputFile := parseFlags()
+
+	input, err := openInputFile(*inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening input file: %v\n", err)
+		os.Exit(1)
+	}
+	defer input.Close()
+
+	output, err := createOutputFile(*outputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating output file: %v\n", err)
+		os.Exit(1)
+	}
+	defer output.Close()
+
+	rowCount, uniqueQuerySequences, uniqueTargetSequences, err := processFile(input, *inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error processing file: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = writeOutput(output, rowCount, uniqueQuerySequences, uniqueTargetSequences)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing output: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func parseFlags() (*string, *string) {
+	inputFile := flag.String("input", "-", "Input file (default: stdin)")
+	outputFile := flag.String("output", "-", "Output file (default: stdout)")
+	flag.StringVar(inputFile, "i", "-", "Input file (default: stdin)")
+	flag.StringVar(outputFile, "o", "-", "Output file (default: stdout)")
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(flag.CommandLine.Output(), "  -i, --input  <file/stdin>\tInput  file (default: stdin)\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  -i, --input  <file/stdin>\tInput   file (default: stdin)\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "  -o, --output <file/stdout>\tOutput file (default: stdout)\n")
-		// flag.PrintDefaults()
 	}
 	flag.Parse()
+	return inputFile, outputFile
+}
 
-	var input *os.File
-	var output *os.File
-	var err error
-
-	if *inputFile == "-" {
-		input = os.Stdin
-	} else {
-		input, err = os.Open(*inputFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error opening input file: %v\n", err)
-			os.Exit(1)
-		}
-		defer input.Close()
+func openInputFile(fileName string) (*os.File, error) {
+	if fileName == "-" {
+		return os.Stdin, nil
 	}
+	return os.Open(fileName)
+}
 
-	if *outputFile == "-" {
-		output = os.Stdout
-	} else {
-		output, err = os.Create(*outputFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating output file: %v\n", err)
-			os.Exit(1)
-		}
-		defer output.Close()
+func createOutputFile(fileName string) (*os.File, error) {
+	if fileName == "-" {
+		return os.Stdout, nil
 	}
+	return os.Create(fileName)
+}
 
+func processFile(input *os.File, inputFileName string) (int, int, int, error) {
 	var scanner *bufio.Scanner
-
-	// Check if the input file is gzip-compressed
-	if strings.HasSuffix(*inputFile, ".gz") {
+	if strings.HasSuffix(inputFileName, ".gz") {
 		gzipReader, err := gzip.NewReader(input)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating gzip reader: %v\n", err)
-			os.Exit(1)
+			return 0, 0, 0, fmt.Errorf("creating gzip reader: %w", err)
 		}
 		defer gzipReader.Close()
 		scanner = bufio.NewScanner(gzipReader)
@@ -67,11 +84,8 @@ func main() {
 	querySequences := make(map[string]struct{})
 	targetSequences := make(map[string]struct{})
 
-	// Process records
 	for scanner.Scan() {
-
 		rowCount++
-
 		line := scanner.Text()
 		fields := strings.Split(line, "\t")
 		if len(fields) < 10 {
@@ -90,13 +104,23 @@ func main() {
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
-		os.Exit(1)
+		return 0, 0, 0, fmt.Errorf("reading input: %w", err)
 	}
 
+	return rowCount, len(querySequences), len(targetSequences), nil
+}
+
+func writeOutput(output *os.File, rowCount, uniqueQuerySequences, uniqueTargetSequences int) error {
 	writer := bufio.NewWriter(output)
-	fmt.Fprintf(writer, "RowCount\t%d\n", rowCount)
-	fmt.Fprintf(writer, "UniqueQuerySequences\t%d\n", len(querySequences))
-	fmt.Fprintf(writer, "UniqueTargetSequences\t%d\n", len(targetSequences))
-	writer.Flush()
+	defer writer.Flush()
+	_, err := fmt.Fprintf(writer, "RowCount\t%d\n", rowCount)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(writer, "UniqueQuerySequences\t%d\n", uniqueQuerySequences)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(writer, "UniqueTargetSequences\t%d\n", uniqueTargetSequences)
+	return err
 }
